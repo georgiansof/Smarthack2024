5b7e9bd9335406a2987f86551e70beb3ee4f25b9
Cosmin <cosmin.calinov@yahoo.com>
2024-11-02 14:33:13 +0200

    Customer class

---
+++ Clase/customer.py
@@ -0,0 +1,8 @@
+class Customer:
+    def __init__(self):
+        self.id = 0;
+        self.max_input = 0.0
+        self.over_input_penalty = 0.0
+        self.late_delivery_penalty  = 0.0
+        self.early_delivery_penalty = 0.0
+        
