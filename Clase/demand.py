9272b51ffed749a348508f91319e6fd3cfcb2c5b
Adrian Fota <adrian.mihael.fota@gmail.com>
2024-11-02 15:12:37 +0200

    Deleted Clase/

--- Clase/demand.py
+++
@@ -1,8 +0,0 @@
-class Demand:
-    def __init__(self):
-        self.id = 0
-        self.customer_id = 0
-        self.quantity = 0.0
-        self.start_delivery_day = 0
-        self.end_delivery_day = 0
-        
