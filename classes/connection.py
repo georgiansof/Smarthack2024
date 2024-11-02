664aefadff75d3c81359878bc56dcfd8bacd58af
Cosmin <cosmin.calinov@yahoo.com>
2024-11-02 16:32:56 +0200

    Modify

--- classes/connection.py
+++ classes/connection.py
@@ -11,12 +11,12 @@ class Connection():
         _lead_time_days, _max_capacity, _connection_type
         ):
         
-        self.id = 0
-        self.from_id = 0
-        self.distance = 0.0
-        self.lead_time_days = 0
-        self.max_capacity = 0.0
-        self.connection_type = ConnectionType.UNKNOWN
+        self.id = _id
+        self.from_id = _from_id
+        self.distance = _distance
+        self.lead_time_days = _lead_time_days
+        self.max_capacity = _max_capacity
+        self.connection_type = _connection_type
         
     def __str__(self):
         return (f"Connection(id={self.id}, from_id={self.from_id},
