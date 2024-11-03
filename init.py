e206f6165b8bfee2ba1de288bab573bc1a1b8084
Stefan <dinca.stefan960@gmail.com>
2024-11-03 07:30:54 +0200

    200k build

--- init.py
+++ init.py
@@ -55,3 +55,55 @@ def initObj():
         demands.append(Demand(*row))
 
     return refineries, customers, tanks, connections, demands
+
+def initObjAPI():
+    # Refineries
+    refineries = []
+
+    refineries_csv = "./data/refineries.csv"
+    fields, rows = csvparser(refineries_csv)
+
+    for row in rows:
+        refineries.append(Refinery(*row[:-1]))
+
+    # Customers
+    customers = []
+
+    customers_csv = "./data/customers.csv"
+    fields, rows = csvparser(customers_csv)
+
+    for row in rows:
+        customers.append(Customer(*row[:-1]))
+
+
+    # Tanks
+    tanks = [] 
+
+    tanks_csv = "./data/tanks.csv"
+    fields, rows = csvparser(tanks_csv)
+    
+    for row in rows:
+        tanks.append(Tank(*row[:-1]))
+
+    
+    # Connections
+    connections = []
+
+    connections_csv = "./data/connections.csv"
+    fields, rows = csvparser(connections_csv)
+
+    for row in rows:
+        connections.append(Connection(*row))
+        
+    # Demands
+    demands = []
+
+    demands_csv = "./data/demands.csv"
+    fields, rows = csvparser(demands_csv)
+
+    for row in rows:
+        if(int(row[3]) == 0):
+            demands.append(Demand(*row))
+
+    return refineries, customers, tanks, connections, demands
+
